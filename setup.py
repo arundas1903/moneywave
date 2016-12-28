e252b99a43ac44a1812059721374eab2aac2361f
Arundas <arundas1903@gmail.com>
2016-12-28 15:55:36 +0530

    Add license and setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'moneywave',
+  packages = ['moneywave'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Integrate library for moneywave',
+  author = 'Arundas R and Muhammed Riyas',
+  author_email = 'arundas1903@gmail.com',
+  url = '', # use the URL to the github repo
+  download_url = '', # I'll explain this in a second
+  keywords = ['moneywave'], # arbitrary keywords
+  classifiers = [],
+)
